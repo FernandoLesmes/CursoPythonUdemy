00295c0c62a5c24b368d91a22af7b47cfd7c6f64
Fernando lesmes <lesmesfernando6@gmail.com>
2025-11-04 22:36:52 -0500

    Primer commit - A  mi rama

--- 12.funciones/ejercicio1.py
+++ 12.funciones/ejercicio1.py
@@ -11,6 +11,20 @@ def  numero ():
         lista.append(num)
         i += 1
 
+def ordenar ():
+    lista.sort()
+    pares = []
+    impares = []
+    for i in lista:
+        if i % 2 == 0:
+            pares.append(i)
+        else:
+            impares.append(i)
+    
+    print(pares)
+    print(impares)
+
 numero()
-print (lista)
+ordenar()    
+
 
